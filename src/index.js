836984be88e9c14ec669a7b14ee0e2f7e1d82238
Boyan Dimitrov <boyan.dimitrov@scalefocus.com>
2019-04-22 15:23:31 +0300

    configurations

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ import { Router, Route, Switch } from "react-router-dom";
 import "./assets/scss/material-kit-react.scss?v=1.4.0";
 
 // pages for this product
-import Home from "views/Home/Home.jsx";
-import ComponentsExamples from "views/Components/ComponentsExamples.jsx";
-import LandingPage from "views/LandingPage/LandingPage.jsx";
-import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
-import LoginPage from "views/LoginPage/LoginPage.jsx";
-import Info from "views/Info/Info.jsx";
-import Bagpipe from "views/Bagpipe/Bagpipe.jsx";
-import Bagpipers from "views/Bagpipers/Bagpipers.jsx";
-import Activities from "views/Activities/Activities.jsx";
-import Contacts from "views/Contacts/Contacts.jsx";
-import Participations from "views/Participations/Participations.jsx";
+import Home from "./views/Home/Home.jsx";
+import ComponentsExamples from "./views/Components/ComponentsExamples.jsx";
+import LandingPage from "./views/LandingPage/LandingPage.jsx";
+import ProfilePage from "./views/ProfilePage/ProfilePage.jsx";
+import LoginPage from "./views/LoginPage/LoginPage.jsx";
+import Info from "./views/Info/Info.jsx";
+import Bagpipe from "./views/Bagpipe/Bagpipe.jsx";
+import Bagpipers from "./views/Bagpipers/Bagpipers.jsx";
+import Activities from "./views/Activities/Activities.jsx";
+import Contacts from "./views/Contacts/Contacts.jsx";
+import Participations from "./views/Participations/Participations.jsx";
 
 var hist = createBrowserHistory();
 
